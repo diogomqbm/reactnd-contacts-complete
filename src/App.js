3adb8d09927342fde0beb02f4d934b7bc08918d7
Diogo Mafra <diogo@Diogos-MacBook-Pro.local>
2018-09-11 16:06:54 -0300

    day one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-
+import ListContacts from './ListContacts'
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  deleteContact = (contact) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(item => item.id !== contact.id)
+    }))
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={this.state.contacts} deleteContact={this.deleteContact}/>
       </div>
     )
   }
